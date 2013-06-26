3f4b7c38b62e6e7bf8a09db123d20943e089ee36
Daniel J. Pritchett <dpritchett@gmail.com>
2013-06-26 14:59:51 -0500

    Pulled redundancies out of my ZPLIILabel class, now it only shows the parts that differ from the parent.

--- lib/endicia/zplii_label.rb
+++ lib/endicia/zplii_label.rb
@@ -5,33 +5,11 @@
 #
 module Endicia
   class ZPLIILabel < Label
-    attr_accessor :image, 
-                  :status, 
-                  :tracking_number, 
-                  :final_postage, 
-                  :transaction_date_time, 
-                  :transaction_id, 
-                  :postmark_date, 
-                  :postage_balance, 
-                  :pic,
-                  :error_message,
-                  :reference_id,
-                  :cost_center,
-                  :request_body,
-                  :request_url,
-                  :response_body
     def initialize(result)
       self.response_body = filter_response_body(result.body.dup)
       data        = result["LabelRequestResponse"] || {}
       encoded_zpl = data["Base64LabelImage"]
       @image      = Base64.decode64 encoded_zpl
-    end
-    
-    private
-    def filter_response_body(string)
-      # Strip image data for readability:
-      string.sub(/<Base64LabelImage>.+<\/Base64LabelImage>/,
-                 "<Base64LabelImage>[data]</Base64LabelImage>")
     end
   end
 end
